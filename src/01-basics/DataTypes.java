7814fe003832ff65d9f1f879cf36b8b3a3aa73a2
sumiran3031 <sumiranpaparkar@gmail.com>
2026-05-28 21:31:11 +0530

    feat: Day 1 - DataTypes

---
+++ src/01-basics/DataTypes.java
@@ -0,0 +1,21 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte b = 100;
+        short s = 5000;
+        int i = 100000;
+        long l = 15000000000L;
+        float f = 3.14f;
+        double d = 9.99999999;
+        char c = 'J';
+        boolean bool = true;
+
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + bool);
+    }
+}
