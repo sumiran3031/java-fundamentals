8f074bb42d9d83fb91cf0867b9cdc35c3d3b009a
sumiran3031 <sumiranpaparkar@gmail.com>
2026-05-28 22:09:16 +0530

    feat: day 1 -Arithmetic  Operators  Example

---
+++ src/01-basics/ArithmeticOperators.java
@@ -0,0 +1,14 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 20, b = 5;
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+        int x = 10;
+        System.out.println("Post increment: " + x++);
+        System.out.println("After increment: " + x);
+        System.out.println("Pre decrement: " + --x);
+    }
+}
