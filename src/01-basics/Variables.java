b282999f252fd49f38bacdeec8a81f583e937d99
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-05-28 21:06:58 +0530

    feat: Day 1 - HelloWorld and project structure

---
+++ src/01-basics/Variables.java
@@ -0,0 +1,15 @@
+public class Variables {
+    public static void main(String[] args) {
+        int age = 20;
+        double salary = 90000.50;
+        char grade = 'A';
+        boolean isStudent = true;
+        String name = "Sumiran";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Student: " + isStudent);
+    }
+}
