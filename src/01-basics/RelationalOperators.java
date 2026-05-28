a6d70d105e23c51bd865fb72823cceb92a439b33
sumiran3031 <sumiranpaparkar@gmail.com>
2026-05-28 21:59:57 +0530

    feat: day 1 -Relational Operator  Example

---
+++ src/01-basics/RelationalOperators.java
@@ -0,0 +1,12 @@
+public class RelationalOperators {
+    public static void main(String[] args) {
+        int a = 10, b = 20;
+
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a >= b: " + (a >= b));
+        System.out.println("a <= b: " + (a <= b));
+    }
+}
