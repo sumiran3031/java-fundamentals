331d3b889d5ce6db8abb0f107bc7512a230a12ea
sumiran3031 <sumiranpaparkar@gmail.com>
2026-05-29 18:59:15 +0530

    feat: Day 2 - IfElse.java

---
+++ src/02-control-flow/IfElse.java
@@ -0,0 +1,18 @@
+public class IfElse{
+    public static void main(String [] args){
+        int marks=75;
+        if (marks>=90) {
+            System.out.println("Grade : A");
+        }
+        else if(marks>75){
+            System.out.println("Grade: B");
+        }
+        else if (marks>=60){
+            System.out.println("Grade: C");
+        }
+        else {
+            System.out.println("Grade: Fail");
+        }
+
+    }
+}
