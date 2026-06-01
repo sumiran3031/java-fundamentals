556ae0bce3491872ca43dd50b8a44ab23d9bc9fd
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-06-01 19:52:57 +0530

    feat : Day 5 - StaticKeyword.java

---
+++ src/05-OOP/StaticKeyword.java
@@ -0,0 +1,19 @@
+class Counter {
+    static int count = 0; 
+    String name;
+    Counter(String name) {
+        this.name = name;
+        count++;
+    }
+    static void showCount() {
+        System.out.println("Total objects: " + count);
+    }
+}
+public class StaticKeyword {
+    public static void main(String[] args) {
+        Counter c1 = new Counter("A");
+        Counter c2 = new Counter("B");
+        Counter c3 = new Counter("C");
+        Counter.showCount();
+    }
+}
