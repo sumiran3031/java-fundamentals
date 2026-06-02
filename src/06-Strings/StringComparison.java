5a41e9a39a39770c5e3328aea2b112749dc98dec
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-06-02 19:31:49 +0530

    feat : Day 6 - StringComparison.java

---
+++ src/06-Strings/StringComparison.java
@@ -0,0 +1,14 @@
+public class StringComparison {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "Hello";
+        String s3 = new String("Hello");
+        String s4 = "hello";
+        System.out.println("s1 == s2: " + (s1 == s2));           
+        System.out.println("s1 == s3: " + (s1 == s3));           
+        System.out.println("s1.equals(s3): " + s1.equals(s3));   
+        System.out.println("equalsIgnoreCase: " + s1.equalsIgnoreCase(s4)); 
+        System.out.println("compareTo: " + s1.compareTo(s4));   
+        System.out.println("compareToIgnoreCase: " + s1.compareToIgnoreCase(s4)); 
+    }
+}
