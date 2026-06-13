22c7b25a20287f6d26c1a16860252a71f1a445d0
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-06-13 18:54:02 +0530

    feat : Day 17 - ParameterizedTest.java

--- src/test/java/17 - Unit Testing (JUnit)/ParameterizedTest.java
+++ src/test/java/17 - Unit Testing (JUnit)/ParameterizedTest.java
@@ -1,3 +1,5 @@
+package unittesting;
+
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -5,25 +7,17 @@ import org.junit.jupiter.params.provider.MethodSource;
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.stream.Stream;
 
-public class ParameterizedTest {
+public class ParameterizedTestDemo {
 
-    boolean isPositive(int n) {
-        return n > 0;
-    }
+    ParameterizedDemo demo = new ParameterizedDemo();
 
-    int square(int n) {
-        return n * n;
-    }
-
-    boolean isPalindrome(String s) {
-        return s.equals(new StringBuilder(s).reverse().toString());
-    }
-    @org.junit.jupiter.params.ParameterizedTest
+    @ParameterizedTest
     @ValueSource(ints = {1, 5, 10, 100})
     void testIsPositive(int number) {
-        assertTrue(isPositive(number));
+        assertTrue(demo.isPositive(number));
     }
-    @org.junit.jupiter.params.ParameterizedTest
+
+    @ParameterizedTest
     @CsvSource({
         "2, 4",
         "3, 9",
@@ -31,20 +25,22 @@ public class ParameterizedTest {
         "5, 25"
     })
     void testSquare(int input, int expected) {
-        assertEquals(expected, square(input));
+        assertEquals(expected, demo.square(input));
     }
-    @org.junit.jupiter.params.ParameterizedTest
+
+    @ParameterizedTest
     @ValueSource(strings = {"madam", "racecar", "level"})
     void testPalindromes(String word) {
-        assertTrue(isPalindrome(word));
+        assertTrue(demo.isPalindrome(word));
     }
+
     static Stream<String> nonPalindromes() {
         return Stream.of("hello", "world", "java");
     }
 
-    @org.junit.jupiter.params.ParameterizedTest
+    @ParameterizedTest
     @MethodSource("nonPalindromes")
     void testNonPalindromes(String word) {
-        assertFalse(isPalindrome(word));
+        assertFalse(demo.isPalindrome(word));
     }
 }
