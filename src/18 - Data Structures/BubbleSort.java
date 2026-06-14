5fcc86944d4f4ca0d243a3eb6a8d38607c2a6f7b
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-06-14 09:55:25 +0530

    feat : Day 18 - BubbleSort.java

---
+++ src/18 - Data Structures/BubbleSort.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class BubbleSort {
+    static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) break; // already sorted
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+
+        System.out.println("Before: " + Arrays.toString(arr));
+        bubbleSort(arr);
+        System.out.println("After:  " + Arrays.toString(arr));
+    }
+}
