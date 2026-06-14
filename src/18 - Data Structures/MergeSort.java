d761ee667746331e630ad94afe88f8109913dc62
Sumiran Paparkar <sumiranpaparkar@gmail.com>
2026-06-14 09:57:16 +0530

    feat : Day 18 - MergeSort.java
    Implement Merge Sort algorithm for sorting an array.

---
+++ src/18 - Data Structures/MergeSort.java
@@ -0,0 +1,40 @@
+import java.util.Arrays;
+
+public class MergeSort {
+    static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int mid = left + (right - left) / 2;
+            mergeSort(arr, left, mid);
+            mergeSort(arr, mid + 1, right);
+            merge(arr, left, mid, right);
+        }
+    }
+
+    static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
+
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        for (int i = 0; i < n1; i++) leftArr[i] = arr[left + i];
+        for (int j = 0; j < n2; j++) rightArr[j] = arr[mid + 1 + j];
+
+        int i = 0, j = 0, k = left;
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) arr[k++] = leftArr[i++];
+            else arr[k++] = rightArr[j++];
+        }
+
+        while (i < n1) arr[k++] = leftArr[i++];
+        while (j < n2) arr[k++] = rightArr[j++];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {38, 27, 43, 3, 9, 82, 10};
+
+        System.out.println("Before: " + Arrays.toString(arr));
+        mergeSort(arr, 0, arr.length - 1);
+        System.out.println("After:  " + Arrays.toString(arr));
+    }
+}
